01d26e5ff57530ebb5e9e951b21599ebce0ccfbb
Christian Navarrete <christian.navarrete@elastic.co>
2025-12-16 10:49:59 +0100

    Update fetch-github-token

--- fetch-github-token/revoke.cjs
+++ fetch-github-token/revoke.cjs
@@ -4,24 +4,7 @@ const fs = require('fs');
 async function revokeToken() {
   try {
     // Read token from GitHub State
-    const stateFile = process.env.GITHUB_STATE;
-    let githubEphemeralToken = null;
-
-    // if (stateFile && fs.existsSync(stateFile)) {
-    //   const stateContent = fs.readFileSync(stateFile, 'utf8');
-    //   const lines = stateContent.split('\n');
-
-    //   for (const line of lines) {
-    //     if (line.startsWith('EPHEMERAL_TOKEN=')) {
-    //       githubEphemeralToken = line.substring('EPHEMERAL_TOKEN='.length).trim();
-    //       break;
-    //     }
-    //   }
-    // }
-
-    if (!githubEphemeralToken && process.env.STATE_EPHEMERAL_TOKEN) {
-      githubEphemeralToken = process.env.STATE_EPHEMERAL_TOKEN;
-    }
+    githubEphemeralToken = process.env.STATE_EPHEMERAL_TOKEN;
 
     if (!githubEphemeralToken) {
       core.info('No GitHub ephemeral token found in inputs, skipping revoke.');
